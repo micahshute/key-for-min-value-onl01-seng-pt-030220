975b92c01579fc3fcc1c810b99c4c908821b97e9
Micah Shute <micah.shute@gmail.com>
2020-03-12 22:50:55 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,10 +7,7 @@ def key_for_min_value(name_hash)
   name_hash.each do |name, num|
     if num < smallest_num 
       # set our variables for the smallest box
-    else 
-      next
     end
-
   end
 end
 
