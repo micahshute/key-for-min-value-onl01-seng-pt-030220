45952801d7da4baff3d67ed9fc89997666a7057d
Micah Shute <micah.shute@gmail.com>
2020-03-12 22:32:00 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,10 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  
 end
 
 
 
 # INPUT --> hash
 # OUTPUT --> Number, which is the smallest value in the input hash
+
+# open each door and check the box sizes. 
+
